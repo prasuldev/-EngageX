187783454a6b91b8830e3ac25ebcbaec23244456
LidhiyaAlmeda <marylidhiya312@gmail.com>
2026-08-19 16:06:46 +0530

    connecting deployed links

--- backend-ai/app/main.py
+++ backend-ai/app/main.py
@@ -28,16 +28,7 @@ app = FastAPI(title="EngageX API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-
-        "http://localhost:3001",
-        "http://127.0.0.1:3001",
-
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-    ],
+    allow_origins=["https://leafy-brioche-7b0536.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
