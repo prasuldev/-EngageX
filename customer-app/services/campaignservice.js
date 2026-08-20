3ed050c76554182339a493f3a63a46bcf3bfad16
LidhiyaAlmeda <marylidhiya312@gmail.com>
2026-08-20 19:55:35 +0530

    removed render and netlify links

--- customer-app/services/campaignservice.js
+++ customer-app/services/campaignservice.js
@@ -1,5 +1,4 @@
-const API_BASE = "https://engagex-3.onrender.com";
-
+const API_BASE = "http://localhost:8000";
 export async function getActiveCampaigns(context = "global") {
     const response = await fetch(
         `${API_BASE}/campaigns/active?context=${context}`
