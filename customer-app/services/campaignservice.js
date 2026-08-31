f34a68e6314c1dd1344d7b1ff93d92ba230333db
LidhiyaAlmeda <144088915+LidhiyaAlmeda@users.noreply.github.com>
2026-08-31 14:40:46 +0530

    Update campaignservice.js

--- customer-app/services/campaignservice.js
+++ customer-app/services/campaignservice.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000";
+const API_BASE = "https://engagex-3.onrender.com";
 export async function getActiveCampaigns(context = "global") {
     const response = await fetch(
         `${API_BASE}/campaigns/active?context=${context}`
